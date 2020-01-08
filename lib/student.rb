f44be8ea7e2522856611218c649c9744e1c45f3a
James Benjamin <james.abenj@gmail.com>
2020-01-08 16:34:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,11 +41,7 @@ class Student
   end
 
   def self.new_from_db(row)
-   student = self.new
-   student.id = row[0]
-   student.name =  row[1]
-   student.grade = row[2]
-   binding.pry
+   student = self.new(row[0], row[1], row[2])
    student
   end
 
